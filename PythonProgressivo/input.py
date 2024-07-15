608a3715923fec7a5a6af478fd078486ee8eaf24
Paulo Henrique dos Santos <paulo.hsantos2@sp.senac.br>
2024-07-15 10:49:08 -0300

    Variaveis,Input

---
+++ PythonProgressivo/input.py
@@ -0,0 +1,8 @@
+variavel=input('Digite algo: ')
+
+print('--------------------------------------------')
+
+idade=input('Qual sua idade? ')
+nome=input('Qual seu nome? ')
+
+print('Olá, seu nome é ', nome, ' e tem ', idade, ' anos de idade! Seja bem vindo ao Curso Python Progressivo')
---
+++ PythonProgressivo/variaveis.py
@@ -0,0 +1,58 @@
+# idade = 18
+
+# texto='Curso Python Progressivo' 
+
+# texto = 'Curso Python Progressivo'
+# print texto
+
+
+# nome='Maria Joaquina de Amaral Pereira Goes'
+# idade=18
+# print nome,idade
+
+# idade=18
+# nome='Maria Joaquina de Amaral Pereira Goes'
+# print('Nome:', nome)
+# print('Idade:', idade)
+
+print ('-------------------------------------------------------------------------')
+
+# Exercício 01:
+# Crie um programa que exiba na tela seu nome completo, sua cidade, estado e data de nascimento.
+
+# Para cada dado desse, crie uma variável apropriada. Use nomes que façam sentido ('cidade', 'estado' etc, nada de ficar criando variáveis com nomes 'a', 'b', 'x' ou 'y' - isso é um péssimo hábito entre programadores).
+
+# Exiba tudo na tela, bonitinho e organizado.
+
+nome = 'ph'
+estado ='sp'
+nascimento = 88
+
+print(nome)
+print(estado)
+print(nascimento)
+
+print('--------------------------------------------------------')
+
+# Crie um programa que exiba na tela o texto 'A melhor banda do mundo é [nome da banda] e a melhor música é [nome da música]'.
+
+# O nome da banda e o nome da música devem estar declarados em duas variáveis diferentes.
+
+# A saída deve estar assim:
+
+bestBand = 'rancid'
+print ('A melhor banda do mundo é' ,bestBand )
+
+print('--------------------------------------------------')
+
+salario=0.0
+print('Antes de ser programdor eu ganhava R$',salario,'por mes')
+
+salario=5000.0
+print('Agora virei programador Python e ganho R$',salario)
+
+print('------------------------------------------------------------')
+
+import keyword
+print(keyword.kwlist)
+
